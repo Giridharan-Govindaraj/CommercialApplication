114fcdb8d232371b814732674f4d031c3c5b74ef
Giridharan-Govindaraj <mr.giridharangovindharaj@gmail.com>
2024-04-06 14:26:30 +0530

    Initial project setup

---
+++ CommercialApplication/src/main/java/github/com/GiridharanGovindaraj/CommercialApplication/CommercialApplication.java
@@ -0,0 +1,13 @@
+package github.com.GiridharanGovindaraj.CommercialApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercialApplication.class, args);
+	}
+
+}
---
+++ CommercialApplication/src/test/java/github/com/GiridharanGovindaraj/CommercialApplication/CommercialApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.GiridharanGovindaraj.CommercialApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
